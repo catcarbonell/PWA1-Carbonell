c73247d3d52a88e41f8e166b65308c55fd632436
Cathleen Carbonell <catcarbonell@gmail.com>
2014-09-03 16:37:30 -0700

    Wk1 - Duel - Setup
    Setting up the files needed for this project.

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,16 @@
+//Functions - Basic Structure
+
+function outptMsg(){
+        console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+calcArea();
+outptMsg();
+
