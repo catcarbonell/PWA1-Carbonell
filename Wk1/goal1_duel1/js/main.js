fba6ef406c3d766ae8c498bc79cec08ab9638db4
Cathleen Carbonell <catcarbonell@gmail.com>
2014-09-04 12:37:40 -0700

    Wk1- Duel 1 - Function Test
    Following the video, tested out function- console.log print -FIGHT!!

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -1,3 +1,12 @@
-/** Cathleen Carbonell
- * Date: 9/07/2014
+/*
+Name: Cathleen Carbonell
+Date: 9/07/2014
+Assignment: Goal1: Assignment: Duel1
  */
+
+//self-executing function
+(function(){
+
+    console.log("FIGHT!!");
+
+})();
